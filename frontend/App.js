06ce08ed8607c2190ebc96f3e16177cd82f2618a
Thanuja_313 <sewmini234@gmail.com>
2026-08-11 16:37:08 +0530

    Refactor App component structure and update Gradle properties for JDK path

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { View, Text } from 'react-native';
 
 const App = () => {
- 
-}
+  return (
+    <View>
+      <Text style={{ top: 50, fontSize: 30, fontWeight: 'bold' }}>
+        Mind Matter
+      </Text>
+    </View>
+  );
+};
 
-export default App
+export default App;
