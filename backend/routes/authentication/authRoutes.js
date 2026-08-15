98601e8aa87040c21de59d8f201ed45f14748c38
Vihal Arumasinghe <vihal606@gmail.com>
2026-08-15 10:22:04 +0530

    Added the login funciton with token generation

--- backend/routes/authentication/authRoutes.js
+++ backend/routes/authentication/authRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { registerUser } from '../../controllers/authentication/authController.js'
+import {
+    registerUser,
+    loginUser
+} from '../../controllers/authentication/authController.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 export default router
