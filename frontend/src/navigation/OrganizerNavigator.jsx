a3b85c71a66f09792209690574c2459c4ef083c6
Isanka02 <isankasonali7990@email.com>
2026-08-25 18:45:37 +0530

    Update OrganizerNavigator

--- frontend/src/navigation/OrganizerNavigator.jsx
+++ frontend/src/navigation/OrganizerNavigator.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import OrganizerDashboardScreen from '../features/organizer/screens/OrganizerDashboardScreen'
+import CircleDetailScreen from '../features/organizer/screens/CircleDetailScreen'
+import CircleFormScreen from '../features/organizer/screens/CircleFormScreen'
+import ProfileScreen from '../features/profile/screens/ProfileScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -10,6 +13,18 @@ const OrganizerNavigator = () => {
             <Stack.Screen
                 name="OrganizerDashboard"
                 component={OrganizerDashboardScreen}
+            />
+            <Stack.Screen
+                name="CircleDetail"
+                component={CircleDetailScreen}
+            />
+            <Stack.Screen
+                name="CircleForm"
+                component={CircleFormScreen}
+            />
+            <Stack.Screen
+                name="OrganizerProfile"
+                component={ProfileScreen}
             />
         </Stack.Navigator>
     )
