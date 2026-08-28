4db55c35663c04709faa0d78add0acd555ff4c83
Isanka02 <isankasonali7990@email.com>
2026-08-28 22:09:01 +0530

    chore(app): update application entry configuration

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 import {AuthProvider} from './src/context/AuthContext'
 import RootNavigator from './src/navigation/RootNavigator'
 
 const App = () => {
-    console.log('[App] Component rendering')
     return (
-        <AuthProvider>
-            <RootNavigator />
-        </AuthProvider>
+        <SafeAreaProvider style={{flex: 1}}>
+            <AuthProvider>
+                <RootNavigator />
+            </AuthProvider>
+        </SafeAreaProvider>
     )
 }
 
